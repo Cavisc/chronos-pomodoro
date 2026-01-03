0bc1d51c688e48cdbfcf1e05fea68d658f47aa58
Cavisc <cavisc.icm@gmail.com>
2026-01-02 21:25:53 -0300

    Add form validation and create a new task

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,13 +3,49 @@ import { Button } from '../Button';
 import { Cycles } from '../Cycles';
 import { Input } from '../Input';
 import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 
 export function MainForm() {
-  const taskName = useRef<HTMLInputElement>(null);
+  const { setState } = useTaskContext();
+
+  const taskNameInput = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log('Submitted:', taskName.current?.value);
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1,
+        secondsRemaining,
+        formattedSecondsRemaining: '00:00',
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
   }
 
   return (
@@ -20,7 +56,7 @@ export function MainForm() {
           labelText='task:'
           type='text'
           placeholder='Ex.: estudar para a prova'
-          ref={taskName}
+          ref={taskNameInput}
         />
       </div>
 
